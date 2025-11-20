1b5bfb886997bfda0a65d94091e8f8981c731fca
Cesar <cesarberger72@gmail.com>
2025-11-20 11:00:50 -0300

    1er Avance

---
+++ Resumen.py
@@ -0,0 +1,23 @@
+#### 1 Clase "Asiento"
+
+## atributos
+'numero_de_asiento' Id del asiento
+'disponibilidad' si el asiento esta disponible en booleano
+'precio' : precio del asiento dependiendo del horario
+## Metodos
+'__init__': constructor
+'reservar()': cambia el estado de disponible a falso
+'liberar()': suelta el asiento
+ 
+#### 2 Clase "Embarcacion" 
+
+## atributos
+
+
+##se seguira haciendo luego
+
+ 
+
+
+
+
